a07802cee84dc44fee534780fbdfa09e79c0ad88
akifnadeem <akifnadeem29@gmail.com>
2021-04-04 03:40:09 +0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -58,6 +58,15 @@ document.querySelector('#currentRoom').innerText = `Current room is ${roomId} - 
   });
 
   // Code for creating a room below
+  roomRef.onSnapshot(async snapshot -> {
+    console.log('Got updated room:', snapshot.data());
+    const data = snapshot.data();
+    if (!peerConnection.currentRemoteDescription && data.answer) {
+        console.log('Set remote description: ', data.answer);
+        const answer = new RTCSessionDescription(data.answer)
+        await peerConnection.setRemoteDescription(answer);
+    }
+});
 
   // Code for creating a room above
 
